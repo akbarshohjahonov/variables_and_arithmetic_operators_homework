1e47a9e8b826aaa70a8f06a900c2b8685dfa10b5
akbarshohjahonov <akbarshohjahonov@akbarshohjahonov@gmail.com>
2022-11-14 13:54:31 +0500

    uchisnch

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -3,6 +3,6 @@ a=6
 # Create a variable "b"  and assign it the value of the int 2.
 b=2
 # Create a variable called 'answer'.
-javob=(6-2)
+answer=(6-2)
 # find the difference of a and b then assign to "answer"
-print (javob)
+print (answer)
